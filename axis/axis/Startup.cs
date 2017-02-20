0ecbcc7c51000998847ec8a45d6734fba80eb4be
josean14 <jagr14@gmail.com>
2017-02-19 20:10:03 -0600

    Inicio Proyecto
    Inicia proyecto

---
+++ axis/axis/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(axis.Startup))]
+namespace axis
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
